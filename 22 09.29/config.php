bf6fa5b0a5515be957c114c21e1280d7765479b7
IevaKleX <42797881+IevaKleX@users.noreply.github.com>
2018-10-01 10:03:08 +0300

    var_dump ($con);

--- 22 09.29/config.php
+++ 22 09.29/config.php
@@ -15,13 +15,13 @@ $db = "ievakle";
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $db);
 
-// var_dump ($con);
+var_dump ($con);
 
 // Check connection
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
 }
-    // echo "Connected successfully"
+    echo "Connected successfully"
 ?>
 
 
